d5bfec5a347b0fc15fb98d7728264502738a6bfd
Benjamin André <andre.vincent.benjamin@gmail.com>
2024-07-08 16:05:37 +0200

    Added clearscreen

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,44 @@
 use proc_macro::TokenStream;
-use syn::{parse_macro_input, Ident, LitInt, ItemFn};
+use syn::{
+    parse::{Parse, ParseStream},
+    parse_macro_input, Ident, ItemFn, LitInt,
+};
 
-struct AocMacroInput (LitInt, LitIn);
+struct AocMacroInput(LitInt, LitInt);
+
+impl Parse for AocMacroInput {
+    fn parse(input: ParseStream) -> Result<Self, syn::Error> {
+        let year = input.parse()?;
+        let day = input.parse()?;
+        Ok(AocMacroInput(year, day))
+    }
+}
 
 #[proc_macro_attribute]
 pub fn main(args: TokenStream, input: TokenStream) -> TokenStream {
-  let aoc_macro_input = parse_macro_input!(args as AocMacroInput);
-  let (day, year) = aoc_macro_input;
-  let input_path = format!("../../inputs/{}_{}.in",year.token(), day.token());
+    let aoc_macro_input = parse_macro_input!(args as AocMacroInput);
+    let year = aoc_macro_input.0;
+    let day = aoc_macro_input.1;
+    let input_path = format!("../../inputs/{}_{}.in", year.token(), day.token());
 
-  let mut aoc_solution = parse_macro_input!(input as ItemFn);
-  aoc_solution.sig.ident = Ident::new("aoc_solution", aoc_solution.sig.ident.span());
+    let mut aoc_solution = parse_macro_input!(input as ItemFn);
+    aoc_solution.sig.ident = Ident::new("aoc_solution", aoc_solution.sig.ident.span());
 
-  let tokens = quote::quote! {
-    const INPUT: &str = include_str!(#input_path);
-    #aoc_solution
-    fn main() {
-      let now = ::std::time::Instant::now();
-      let (p1, p2) = aoc_solution(INPUT.trim_end());
-      let elapsed = now.elapsed();
-      println!("Part one: {}", p1);
-      println!("Part two: {}", p2);
-      if elapsed.as_millis() > 0 {
-        println!("Time: {}ms", elapsed.as_millis());
-      } else {
-        println!("Time: {}μs", elapsed.as_micros());
+    let tokens = quote::quote! {
+      const INPUT: &str = include_str!(#input_path);
+      #aoc_solution
+      fn main() {
+        let now = ::std::time::Instant::now();
+        let (p1, p2) = aoc_solution(INPUT.trim_end());
+        let elapsed = now.elapsed();
+        println!("Part one: {}", p1);
+        println!("Part two: {}", p2);
+        if elapsed.as_millis() > 0 {
+          println!("Time: {}ms", elapsed.as_millis());
+        } else {
+          println!("Time: {}μs", elapsed.as_micros());
+        }
       }
-    }
-  };
-  TokenStream::from(tokens)
+    };
+    TokenStream::from(tokens)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,29 @@
-use std::thread;
-use std::sync::Arc;
-use std::time::{Duration, Instant};
 use std::cmp::min;
 use std::io::{self};
+use std::sync::Arc;
+use std::thread;
+use std::time::{Duration, Instant};
 
-use rand::Rng;
 use aoc_client::AocClient;
 use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
+use rand::Rng;
 
 struct AocDay<'a> {
-    year : i32,
-    day : u32,
-    part_one : &'a Option::<(i64, Box::<dyn Fn(&str) -> String>)>, 
-    part_two : &'a Option::<(i64, Box::<dyn Fn(&str) -> String>)>,
+    year: i32,
+    day: u32,
+    part_one: &'a Option<(i64, Box<dyn Fn(&str) -> String>)>,
+    part_two: &'a Option<(i64, Box<dyn Fn(&str) -> String>)>,
 }
 
 trait Tryable {
-    fn try_part(&self, part : &Option::<(i64, Box::<dyn Fn(&str) -> String>)>) -> Result<u64, String>;
+    fn try_part(&self, part: &Option<(i64, Box<dyn Fn(&str) -> String>)>) -> Result<u64, String>;
 }
 
 impl Tryable for AocClient {
-    fn try_part(&self, part : &Option::<(i64, Box::<dyn Fn(&str) -> String>)>) -> Result<u64, String> { 
+    fn try_part(&self, part: &Option<(i64, Box<dyn Fn(&str) -> String>)>) -> Result<u64, String> {
         let real_part = match part {
             Some(p) => p,
-            None => return Err(String::from("Part not available"))
+            None => return Err(String::from("Part not available")),
         };
         let _input = match self.get_input() {
             Ok(s) => s,
@@ -32,13 +32,13 @@ impl Tryable for AocClient {
         let func = &real_part.1;
         let now = Instant::now();
         let solution = func(&_input);
-        let time: u64 = match now.elapsed().as_micros().try_into(){
+        let time: u64 = match now.elapsed().as_micros().try_into() {
             Ok(n) => n,
             Err(s) => return Err(s.to_string()),
         };
         let answer = match self.submit_answer(real_part.0, solution) {
             Ok(so) => so,
-            Err(aoc_error) => return Err(aoc_error.to_string()), 
+            Err(aoc_error) => return Err(aoc_error.to_string()),
         };
         match answer {
             aoc_client::SubmissionOutcome::Correct => return Ok(time),
@@ -53,17 +53,17 @@ trait Runnable {
 
 impl Runnable for AocDay<'_> {
     fn run(&self) -> Vec<u64> {
-        let mut solutions : Vec<u64> = Vec::new();
+        let mut solutions: Vec<u64> = Vec::new();
         let mut binding = AocClient::builder();
         let client = match binding.session_cookie_from_default_locations() {
             Ok(c) => c,
             _ => return solutions,
         };
-        let client = match client.year(self.year)  {
+        let client = match client.year(self.year) {
             Ok(c) => c,
             _ => return solutions,
         };
-        let client = match client.day(self.day)  {
+        let client = match client.day(self.day) {
             Ok(c) => c,
             _ => return solutions,
         };
@@ -88,8 +88,8 @@ fn main() {
     let total_size = 50;
     let starting_year = 2015;
     let years = 9;
-    let mpb = MultiProgress::new();
-    let mut pbs : Vec<Arc<ProgressBar>> = vec![];
+    let mpb : MultiProgress = MultiProgress::new();
+    let mut pbs: Vec<Arc<ProgressBar>> = vec![];
 
     for i in 0..years {
         let pb = mpb.add(ProgressBar::new(total_size));
@@ -101,7 +101,7 @@ fn main() {
 
     for i in 0..years {
         let pb = pbs[i].clone();
-        threads.push(thread::spawn(move || { 
+        threads.push(thread::spawn(move || {
             let max_stars = rand::thread_rng().gen_range(0..total_size);
             while downloaded < max_stars {
                 let pos = pb.position();
@@ -118,9 +118,10 @@ fn main() {
         let _ = thread.join();
     }
 
-    let total_stars : u64 = pbs.iter().map(|apb| apb.position()).sum();
+    let total_stars: u64 = pbs.iter().map(|apb| apb.position()).sum();
     println!("Congratulations on getting all those {}★ !", total_stars);
 
     let mut input = String::new();
-    let _ = io::stdin().read_line(&mut input).unwrap();
+    let _ = io::stdin().read_line(&mut input);
+    let _ = clearscreen::clear();
 }
